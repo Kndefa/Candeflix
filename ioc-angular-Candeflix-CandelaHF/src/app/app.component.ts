dcb62f93364ee14c9ab9d342ffe6d6e620eba478
Kndefa <cande_fano@hotmail.com>
2026-03-03 18:42:11 +0100

    se añade constructor, estilos y html

--- ioc-angular-Candeflix-CandelaHF/src/app/app.component.ts
+++ ioc-angular-Candeflix-CandelaHF/src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ioc-angular-Candeflix-CandelaHF';
+
+  constructor() {
+    console.log('Candeflix ha inicializado correctamente!');
+  }
 }
